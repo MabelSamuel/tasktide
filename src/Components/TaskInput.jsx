1bcecda65e73142e539dc9a0e3fa8913aa29bdb5
krestwastaken <martynskelvin@gmail.com>
2025-03-02 21:30:55 +0100

    task object

--- src/Components/TaskInput.jsx
+++ src/Components/TaskInput.jsx
@@ -1,14 +1,30 @@
-import React, { useState } from 'react'
+import React, { useState } from 'react';
+import { v4 as uuidv4 } from 'uuid';
 
 import TaskList from './TaskList';
 
 export default function TaskInput({ initialTasks, setInitialTasks }) {
-    const [ task, setTask ] = useState('');
+    const [ task, setTask ] = useState({
+        name:'',
+        id: uuidv4(),
+        completed: false
+    });
 
     const handleSubmit = (e) => {
         e.preventDefault();
         setInitialTasks([...initialTasks, task]);
-        setTask('');
+        setTask({
+            name:'', 
+            id: uuidv4(), 
+            completed: false
+        });
+    }
+
+    const handleChange = (e) => {
+        setTask({
+            ...task,
+            name: e.target.value
+        });
     }
 
     return (
@@ -17,8 +33,8 @@ export default function TaskInput({ initialTasks, setInitialTasks }) {
 
                 <input 
                  type='text'
-                 value={task}
-                 onChange={(e) => setTask(e.target.value)}
+                 value={task.name}
+                 onChange={handleChange}
                  placeholder='Enter task'
                 />
                 
