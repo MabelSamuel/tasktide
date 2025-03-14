4d7497bfee7c317c1e618658cc5210248a760f41
Krest <martynskelvin@gmail.com>
2025-03-14 20:07:07 +0100

    Compeleted style 2

--- src/Components/CompletedTasks.jsx
+++ src/Components/CompletedTasks.jsx
@@ -11,20 +11,22 @@ export default function CompletedTasks({ tasks }) {
   }
 
   return (
-    <div className="mt-6 p-3 bg-green-50 rounded-sm w-3/4 mx-auto">
-      <div className='flex items-center' onClick={toggleDropdown}>
+    <div className="w-3/4 mx-auto mt-5">
+      <div className='flex items-center cursor-pointer mt-2 p-2 bg-green-50 rounded-sm w-1/3' onClick={toggleDropdown}>
         <button className='text-gray-500 hover:text-gray-700'>
           {isOpen ? <IoChevronDown/> : <IoChevronForward/>}
         </button>
         <h2 className="font-semibold text-gray-500 mb-0 ml-2">
-          Completed Tasks <span className='px-2 py-1 text-sm'>{completedTasks.length}</span>
+          Completed Tasks <span className='px-2 py-1 text-sm'>
+            {completedTasks.length}
+          </span>
         </h2>     
       </div>
-      
+
       {isOpen && (
-        <div className='mt-3'>
+        <div className="mt-1">
           {completedTasks.length === 0 ? (
-            ''
+            <div className="p-2 bg-green-50 rounded-sm text-gray-500 italic">No completed tasks</div>
           ) : (
             <div className='space-y-2'>
               {completedTasks.map(task => (
